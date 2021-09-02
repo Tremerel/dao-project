62b74038572cdfdb4916ea0033811de1694bc74f
Tremerel <84023901+Tremerel@users.noreply.github.com>
2021-09-02 11:17:09 +0200

    Add files via upload

---
+++ daophp/classes/DAO_server.php
@@ -0,0 +1,10 @@
+<?php
+const DB_HOST = 'localhost';
+const DB_NAME = 'id17451623_test';
+const DB_USER = 'id17451623_vinkeke';
+const DB_PASS = '19gF8/UhY8i403u)';
+
+// $sname= "localhost";
+// $unmae="id17451623_vinkeke";
+// $password = "19gF8/UhY8i403u)";
+// $db_name = "id17451623_test";
---
+++ daophp/index.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+
+if(isset($_SESSION['id']) && isset($_SESSION['email'])){
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <title>Car Table</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/table-css.css">
+  <script src="js/modal.js" defer></script>
+  <script src="js/app.js" defer></script>
+</head>
+
+<body>
+<div class="container">
+  <h1 id="title">~ Car Record Input ~</h1>
+  <div class="deco">
+  <h3>Hello, <?php echo $_SESSION['name'];?>  </h3><br>
+   <a href="logout.php" class="button">Déconnexion</a></div>
+  </div>
+
+  <div class="container">
+    <div id="input">
+      <h1 class="heading">Register A Car</h1>
+      <form name="formCar" method="post" action="" id="formulaire">
+
+
+        <label for="name">Registration</label>
+        <input type="text" name="registration" id="registration" class="inputbox">
+
+        <label for="name">Colour</label>
+        <input type="text" name="colour" id="colour" class="inputbox">
+
+        <label for="name">Make</label>
+        <input type="text" name="make" id="make" class="inputbox">
+
+        <label for="name">Model</label>
+        <input type="text" name="model" id="model" class="inputbox">
+        <button class="inputbox" id="addcarbutton" type="submit">Add Car</button>
+        <div id="confirmation"></div>
+      </form>
+
+    </div>
+  </div>
+  <div class="table-container">
+    <h1 class="heading">Registred Car</h1>
+    <table class="table" id="cartable">
+      <thead>
+        <tr>
+          <th>Regist</th>
+          <th>Colour</th>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Changes</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+
+    </table>
+  </div>
+
+
+  <!-- The Modal -->
+  <div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <p>Are you sure you want to delete this car?</p>
+      <button id="yes">Yes</button>
+      <button id="cancel">Cancel</button>
+    </div>
+
+  </div>
+
+
+</body>
+
+</html>
+
+<?php
+}else{
+    header("Location: ../index.php");
+    exit();
+}
+?>
